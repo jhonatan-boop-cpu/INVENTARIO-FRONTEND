cef65dfc0aec28a77e72b3df2319b7fef20e54d7
jhonatancancarip <jhonatancancarip@gmail.com>
2025-10-03 11:18:20 -0400

    FROND ACTUAL

--- invent/Scriptss/Global.js
+++
@@ -1,8 +0,0 @@
-/*FUNCION BOTON HAMBURGUESA*/ 
-const btn = document.getElementById("menuBtn");
-  const menu = document.querySelector(".fondo_menu");
-
-  btn.addEventListener("click", () => {
-    menu.classList.toggle("open");
-  });
-  
